1449e38a6300ed3386df8592c9f65497c467483a
yuyang <yy@mailizc.com>
2018-08-07 14:27:54 +0800

    Initial Commit

---
+++ Try_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Try_Swift
+//
+//  Created by 好多余先生 on 2018/8/7.
+//  Copyright © 2018年 好多余先生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
